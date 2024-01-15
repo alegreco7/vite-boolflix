df17a5607e1ef6324d65e2ed2c5e3ade920d1003
alegreco7 <alexgreco2002@gmail.com>
2024-01-15 10:21:11 +0100

    link new component to app.vue

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,14 @@
 <script>
 import AppHeader from './components/AppHeader.vue';
+import AppFilms from "./components/AppFilms.vue";
+import AppTv from './components/AppTv.vue';
 import { store } from "./store";
 import axios from 'axios';
 export default {
   components: {
-    AppHeader
+    AppHeader,
+    AppFilms,
+    AppTv
   },
   data() {
     return {
@@ -13,8 +17,14 @@ export default {
   },
   methods: {
     search_title() {
+      store.arrayFilms = []
       axios.get(`${store.urlFilms}${store.APIKey}&query=${store.searchValue}&language=it-IT`).then(response => {
-        console.log(response.data.results);
+        store.arrayFilms = [...response.data.results];
+      })
+      store.arrayTv = []
+      axios.get(`${store.urlTv}${store.APIKey}&language=it-IT&query=${store.searchValue}`).then(data => {
+        store.arrayTv = [...data.data.results];
+        console.log(store.arrayTv);
       })
     }
   },
@@ -22,6 +32,10 @@ export default {
 </script>
 <template lang="">
   <AppHeader @SearchTitle="search_title"/>
+  <div class="container">
+    <AppFilms/>
+    <AppTv/>
+  </div>
 </template>
 <style lang="scss">
 @use './styles/generals.scss';
