48f8e99cb038d678e1d2fa47a6fa89a3b3bb943a
alegreco7 <alexgreco2002@gmail.com>
2024-01-15 09:38:53 +0100

    header complete

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,27 @@
 <script>
+import AppHeader from './components/AppHeader.vue';
+import { store } from "./store";
+import axios from 'axios';
 export default {
-
+  components: {
+    AppHeader
+  },
+  data() {
+    return {
+      store
+    }
+  },
+  methods: {
+    search_title() {
+      axios.get(`${store.urlFilms}${store.APIKey}&query=${store.searchValue}&language=it-IT`).then(response => {
+        console.log(response.data.results);
+      })
+    }
+  },
 }
 </script>
 <template lang="">
-  <div>
-
-  </div>
+  <AppHeader @SearchTitle="search_title"/>
 </template>
 <style lang="scss">
 @use './styles/generals.scss';
--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -4,11 +4,36 @@ export default {
 }
 </script>
 <template lang="">
-    <div>
-
-    </div>
+    <header>
+        <div class="container">
+            <div class="row justify-content-between">
+                <div class="col-2">
+                    <div class="logo">
+                        <img src="https://upload.wikimedia.org/wikipedia/commons/0/08/Netflix_2015_logo.svg" alt="logo netflix">
+                    </div>
+                </div>
+                <div class="col-6">
+                    <div class="input-group m-3">
+                        <input type="text" class="form-control" placeholder="Film, SerieTv">
+                        <button class="btn btn-secondary">Cerca</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </header>
 </template>
 <style lang="scss" scoped>
 @use '../styles/partials/variables' as *;
 @use '../styles/generals.scss';
+
+header {
+    padding: 20px 0;
+    background-color: $black;
+
+    .logo {
+        display: flex;
+        align-items: center;
+        height: 100%;
+    }
+}
 </style>
