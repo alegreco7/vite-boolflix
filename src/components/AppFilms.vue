ce23c88562c2e08edb73a8d0d99d52039b38e5ae
alegreco7 <alexgreco2002@gmail.com>
2024-01-15 10:21:39 +0100

    vote films

--- src/components/AppFilms.vue
+++ src/components/AppFilms.vue
@@ -25,10 +25,10 @@ export default {
             <ul class="list-unstyled">
                 <li>{{ film.title }}</li>
                 <li>{{ film.original_title }}</li>
-                <li>{{ film.vote_average }}</li>
                 <li>
                     Lingua originale: <img class="flag" :src="getFlag(film.original_language)" :alt="film.original_language">
                 </li>
+                <li>{{ film.vote_average }}</li>
             </ul>
         </div>
     </div>
@@ -41,4 +41,3 @@ export default {
     width: 20px;
 }
 </style>
-                
